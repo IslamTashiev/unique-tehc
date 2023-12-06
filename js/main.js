ff3314d4d7c6a845778f0c0b09ba44bce7149674
-g <-g>
2023-12-06 13:18:16 +0600

    finish

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const navSlide = function () {
+	const burger = document.querySelector(".burger");
+	const nav = document.querySelector(".links_list");
+	const navLinks = document.querySelectorAll(".links_list li");
+
+	burger.addEventListener("click", function () {
+		//Toggle nav
+		nav.classList.toggle("nav-active");
+
+		//Burder animation
+		burger.classList.toggle("toggle");
+	});
+};
+navSlide();
